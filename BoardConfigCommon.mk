f1c52425fc58e6e77e5e990899ff46bb8dd2e0a1
Arungz <gzarun5@gmail.com>
2020-11-28 11:00:45 +0530

    sm6150-common: Remove libwinder and libtransport

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -201,7 +201,7 @@ VENDOR_SECURITY_PATCH := 2020-07-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
